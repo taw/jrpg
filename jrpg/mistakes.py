896797c12aecddb7a95c5b91ffa0d901b186f5ec
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-02-11 13:36:10 +0000

    If cannot open mistakes file (usually read only dir after unzipping), print error on stdout

--- jrpg/mistakes.py
+++ jrpg/mistakes.py
@@ -8,11 +8,21 @@ class Mistakes:
     def __init__(self):
         y,mo,d,h,mi,s,wd,yd,isdst = time.localtime()
         self.file_name = "mistakes-%04d-%02d-%02d-%02d-%02d-%02d.txt" % (y,mo,d,h,mi,s)
+        self.opened = False
         self.fh = None
     def open(self):
-        if not self.fh:
+        if not self.opened:
+            self.opened = True
             self.fh = codecs.open(self.file_name, mode="ab", encoding='utf-8')
+    def save_mistake(self, msg):
+        try:
+            self.open()
+        except IOError as e:
+            print e
+        if self.fh:
+            self.fh.write(msg)
+        else:
+            print msg,
     def mistake(self, attack, soul):
         msg = "Mistake: tried (%s) on demon (%s)\n" % (attack, soul.xp_code())
-        self.open()
-        self.fh.write(msg)
+        self.save_mistake(msg)
