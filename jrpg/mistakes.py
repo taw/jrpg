7f38534b0740cfca1b76872903cd98407b7679f6
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-02-11 13:30:06 +0000

    Only open mistakes file on demand

--- jrpg/mistakes.py
+++ jrpg/mistakes.py
@@ -4,17 +4,15 @@
 import time
 import codecs # Python Unicode Brain Damage
 
-
-# In this otherwise completely straightforward 3-line class
-# we have to deal with Python's Unicode Brain Damage
 class Mistakes:
     def __init__(self):
         y,mo,d,h,mi,s,wd,yd,isdst = time.localtime()
-        fn = "mistakes-%04d-%02d-%02d-%02d-%02d-%02d.txt" % (y,mo,d,h,mi,s)
-        #self.fh = open(fn, "ab")
-        self.fh = codecs.open(fn, mode="ab", encoding='utf-8')
+        self.file_name = "mistakes-%04d-%02d-%02d-%02d-%02d-%02d.txt" % (y,mo,d,h,mi,s)
+        self.fh = None
+    def open(self):
+        if not self.fh:
+            self.fh = codecs.open(self.file_name, mode="ab", encoding='utf-8')
     def mistake(self, attack, soul):
         msg = "Mistake: tried (%s) on demon (%s)\n" % (attack, soul.xp_code())
+        self.open()
         self.fh.write(msg)
-
-
