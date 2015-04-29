9b9dceff37ac2fb6f960f36b4759ac3a1c4318e6
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-04-29 06:45:18 +0100

    Windows package

--- jrpg/setup_windows.py
+++ jrpg/setup_windows.py
@@ -4,7 +4,16 @@
 
 from distutils.core import setup
 import py2exe
+import os
 import glob
+
+origIsSystemDLL = py2exe.build_exe.isSystemDLL
+def isSystemDLL(pathname):
+       if os.path.basename(pathname).lower() in ["sdl_ttf.dll"]:
+               return 0
+       return origIsSystemDLL(pathname)
+py2exe.build_exe.isSystemDLL = isSystemDLL
+
 
 setup(
   windows=["jrpg.py"],
